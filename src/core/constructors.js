340860f642444d3767915ce6f479d87e77384049
Battledash-2 <69129349+Battledash-2@users.noreply.github.com>
2022-01-31 13:38:21 -0500

    optimize

--- src/core/constructors.js
+++ src/core/constructors.js
@@ -2,7 +2,7 @@ const Environment = require('../environment');
 
 module.exports = {
 	String(THIS, env) {
-		return new Environment({
+		return {
 			value: THIS,
 			number: Number(THIS),
 			length: Number(THIS.length),
@@ -10,22 +10,22 @@ module.exports = {
 			substring(from, to) {return THIS.slice(from, to);},
 			split(txt) {return THIS.split(txt);},
 			match(txt) {return THIS.match(new RegExp(txt, 'g'));}
-		});
+		};
 	},
 	Number(THIS, env) {
-		return new Environment({
+		return {
 			value: THIS,
 			string: THIS.toString(),
-		});
+		};
 	},
 	Array(THIS, env) {
-		return new Environment({
+		return {
 			length: THIS.length,
 			lastValue: THIS[THIS.length-1],
 
 			push(v) {THIS.push(v); return THIS;},
 			pop() {THIS.pop(); return THIS;},
 			splice(pos, amo) {THIS.splice(pos, amo); return THIS;},
-		});
+		};
 	}
 };
