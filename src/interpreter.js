3b0e1e2df7497f6b8ff502443ff9578afd6f8465
Battledash-2 <69129349+Battledash-2@users.noreply.github.com>
2022-02-03 19:39:43 -0500

    import js files

--- src/interpreter.js
+++ src/interpreter.js
@@ -251,7 +251,10 @@ class Interpreter {
 
 			let fcontent;
 
-			if (fs.existsSync(path.join(process.cwd(), file))) {
+			if (file.endsWith('.js') && fs.existsSync(path.join(process.cwd(), file))) {
+				const renv = require(path.join(process.cwd(), file));
+				return renv;
+			} else if (fs.existsSync(path.join(process.cwd(), file))) {
 				fcontent = fs.readFileSync(path.join(process.cwd(), file));
 			} else if (fs.existsSync(path.join(__dirname, 'core', 'modules', fname+'.js'))) {
 				const renv = require(path.join(__dirname, 'core', 'modules', fname+'.js'));
